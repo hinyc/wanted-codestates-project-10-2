abcb8c47cb826ab793de310dcd9b53730ad9405f
try <dlsduf@gmail.com>
2022-02-24 19:48:17 +0900

    [Feat] #15 유저정보 전체 배치 및 구도구성

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import OverallRecord from '../Components/OverallRecord';
+import UserinfoBox from '../Components/UserinfoBox';
 
+const username = 'BBEESSTT';
 export default function Main() {
   return (
     <div>
+      <UserinfoBox username={username} />
       <OverallRecord />
     </div>
   );
