92643eee359be2b4c57e599334411e00c3b9c45a
가즈아 <wjsdydxo45@naver.com>
2022-02-24 23:05:51 +0900

    [Feat] hinyc#5 데시보드 순위변동 그래프 구현 및 1대1 매칭 컴포넌트 및 favicon 적용

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import RankChangeChart from '../Components/RankChangeChart';
+import Matching from '../Components/Matching';
 
 export default function Main() {
-  return <div>main</div>;
+  return (
+    <>
+      <Box>
+        <RankChangeChart />
+        <Matching />
+      </Box>
+    </>
+  );
 }
+
+const Box = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
