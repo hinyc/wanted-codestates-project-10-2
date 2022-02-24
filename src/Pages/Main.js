4380f2a1ccffcb86655d68821fe2faff6d913139
try <dlsduf@gmail.com>
2022-02-24 16:33:04 +0900

    [Feat] #11 컴포넌트 구조설정

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import OverallRecord from '../Components/OverallRecord';
 
 export default function Main() {
-  return <div>main</div>;
+  return (
+    <div>
+      <OverallRecord />
+    </div>
+  );
 }
