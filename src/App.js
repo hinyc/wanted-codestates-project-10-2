cade0237db738be4cef884c861c3687c1f28005a
SpearJin <ckdwls2525@gmail.com>
2022-02-25 10:54:56 +0900

    [Feat] #4 매칭박스 - 마크업 & 스타일링 & 필터기능 구현

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './Pages/Main';
 import Ranking from './Pages/Ranking';
+import Maching from './Components/Maching/Match';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/ranking" element={<Ranking />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     {/* <Route path="/" element={<Main />} />
+    //     <Route path="/ranking" element={<Ranking />} /> */}
+    //     <Route path="/maching" element={<Maching />} />
+    //   </Routes>
+    // </Router>
+    <div className="App">
+      <Maching />
+    </div>
   );
 }
 
---
+++ src/Components/Maching/ConditionPage.js
@@ -0,0 +1,160 @@
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+
+export default function ConditionPage() {
+  let dragRef = useRef(null);
+  let dragHandleRef = useRef(null);
+  let positionX = useRef(null);
+  let startPosition = useRef(null);
+
+  const mouseDownHanlder = (e) => {
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    e.preventDefault();
+    startPosition.current = e.clientX;
+    document.onmousemove = move;
+  };
+
+  const move = (e) => {
+    // console.log(startPosition.current, positionX.current, e.clientX);
+    // console.log(positionX.current - e.clientX);
+    if (dragRef.current.getBoundingClientRect().x > e.clientX) {
+      document.onmousemove = null;
+      return;
+    }
+
+    positionX.current = startPosition.current - e.clientX;
+
+    dragHandleRef.current.style.transform = `translateX(calc(-${positionX.current}px))`;
+  };
+
+  return (
+    <Condition>
+      <p className="title">컨디션</p>
+      <ConditionBar>
+        <p>배찌</p>
+        <div className="bar">
+          <div className="statusbar">
+            <div className="base" ref={dragRef}></div>
+            <div className="drag">
+              <div
+                className="drag_handle"
+                ref={dragHandleRef}
+                onMouseDown={mouseDownHanlder}
+                onMouseUp={() => (document.onmousemove = null)}
+                onMouseLeave={() => (document.onmousemove = null)}
+              ></div>
+            </div>
+          </div>
+          <span>{positionX.current}%</span>
+        </div>
+      </ConditionBar>
+      <ConditionBar>
+        <p>메인 상대가 없습니다</p>
+        <div className="bar">
+          <div className="statusbar">
+            <div className="base battle"></div>
+            <div className="drag">
+              <div className="drag_handle"></div>
+            </div>
+          </div>
+          <span>100%</span>
+        </div>
+      </ConditionBar>
+      <div className="fight_btn">
+        <button>Fight</button>
+      </div>
+    </Condition>
+  );
+}
+
+const Condition = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px 20px;
+  background-color: #3a4259;
+  .title {
+    margin: 20px 0 0 10px;
+    color: white;
+    font-size: 14px;
+    text-align: left;
+  }
+  .fight_btn {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    button {
+      width: 100px;
+      height: 30px;
+      margin-top: 10px;
+      border-radius: 20px;
+      border: none;
+      background-color: #3975f9;
+      color: #ffffff;
+      text-align: center;
+      box-shadow: 2px 2px 1px 1px #000000;
+    }
+    button:hover {
+      opacity: 0.8;
+      cursor: pointer;
+    }
+  }
+`;
+
+const ConditionBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #03d387;
+  p {
+    flex: 1.5;
+    padding-left: 10px;
+    font-size: 14px;
+    text-align: left;
+  }
+  .bar {
+    flex: 2.5;
+    display: flex;
+    align-items: center;
+    .statusbar {
+      position: relative;
+      width: 80%;
+      height: 20px;
+      border-radius: 10px;
+      background-color: #ffffff;
+      box-shadow: 2px 2px 1px 1px #000000;
+      .base {
+        width: 100%;
+        height: 100%;
+        border-radius: 10px;
+        background-color: #3975f9;
+      }
+      .drag {
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translateY(-12.5%);
+        .drag_handle {
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
+          background-color: #ffffff;
+          cursor: pointer;
+        }
+      }
+
+      .base.battle {
+        width: 100%;
+        background-color: #f62459;
+        & ~ .drag {
+          left: calc(100% - 30px);
+        }
+      }
+    }
+    span {
+      margin-left: 8px;
+      color: #ffffff;
+      font-size: 14px;
+    }
+  }
+`;
