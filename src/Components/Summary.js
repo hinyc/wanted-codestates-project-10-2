a83c3ab6a5f4017229a2cfca1df1b11ee0006fcd
juo <hwgiwh271@gmail.com>
2022-02-27 17:48:04 +0900

    [Fix] #89 syntaxerror 수정
    -

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -5,39 +5,38 @@ import { TrackSummary } from './TrackSummary';
 // import axios from 'axios';
 // import { headers } from '../Util/util';
 
-function Summary({ matchInfo, nickname }) {
-  const [accesssId, setAccessId] = useState('id');
+// function Summary({ matchInfo, nickname }) {
+//   const [accesssId, setAccessId] = useState('id');
 
-  // console.log(matchInfo);
-  // const fetchUserAccessId = async () => {
-  //   await axios
-  //     .get(`/kart/v1.0/users/nickname/${encodeURI(nickname)}`, headers)
-  //     .then((response) => response.data)
-  //     .then((data) => {
-  //       setAccessId(data.accessId);
-  //     })
-  //     .catch((err) => console.error(err));
-  // };
+// console.log(matchInfo);
+// const fetchUserAccessId = async () => {
+//   await axios
+//     .get(`/kart/v1.0/users/nickname/${encodeURI(nickname)}`, headers)
+//     .then((response) => response.data)
+//     .then((data) => {
+//       setAccessId(data.accessId);
+//     })
+//     .catch((err) => console.error(err));
+// };
 
-  // console.log(matchInfo.data);
-  // const UserData = async () => {
-  //   const response = await axios.get(
-  //     `/kart/v1.0/users/${encodeURI(
-  //       accesssId,
-  //     )}/matches?start_date=&end_date= &offset=0&limit=10&match_types=`,
-  //     headers,
-  //   );
-  //   const userDatas = await response.json();
-  //   console.log(userDatas);
-  // };
+// console.log(matchInfo.data);
+// const UserData = async () => {
+//   const response = await axios.get(
+//     `/kart/v1.0/users/${encodeURI(
+//       accesssId,
+//     )}/matches?start_date=&end_date= &offset=0&limit=10&match_types=`,
+//     headers,
+//   );
+//   const userDatas = await response.json();
+//   console.log(userDatas);
+// };
 
-  // const clickHandler = () => {
-  //   const accessId = fetchUserAccessId();
-  //   const userDatas = UserData();
-  //   console.log('accessId', accessId);
-  //   console.log('userDatas', userDatas);
-  // };
-
+// const clickHandler = () => {
+//   const accessId = fetchUserAccessId();
+//   const userDatas = UserData();
+//   console.log('accessId', accessId);
+//   console.log('userDatas', userDatas);
+// };
 
 function Summary({ matchInfo }) {
   const [clickedTab, setClickedTab] = useState('트랙');
@@ -67,7 +66,6 @@ function Summary({ matchInfo }) {
         </li>
       </TrackKartNav>
       {clickedTab === '트랙' ? (
-
         <TrackSummary />
       ) : (
         <KartSummary matchInfo={matchInfo} />
